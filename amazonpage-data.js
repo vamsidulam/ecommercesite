822e37e407f1f938357fe815a59c89bca790e966
vamsidulam <151850002+vamsidulam@users.noreply.github.com>
2025-05-23 18:44:51 +0530

    Update amazonpage-data.js

--- amazonpage-data.js
+++ amazonpage-data.js
@@ -4,15 +4,16 @@ export const obj=[
         productname:"Men's Casual Cotton Poplin shirt(Regular Fit)",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 17,
-        productcost: 1049
-
+        productcost: 1049,
+        type:'shirt'
     },
     {
         productpic:'amazonpagepics/trackpant.webp',
         productname:"Lymio Track Pant for Men || Track pants || Plain Track Pant(TP-57-58)",
         productratingpic: 'amazonpagepics/rating-30.png',
         productrating: 100,
-        productcost: 568
+        productcost: 568,
+        type:'pant'
 
     },
     {
@@ -20,7 +21,8 @@ export const obj=[
         productname:"Lymio Men Cargo || Men Cargo pants || Cotton(Cargo-70-73)",
         productratingpic: 'amazonpagepics/rating-45.png',
         productrating: 423,
-        productcost: 699
+        productcost: 699,
+        type:'pant'
 
     },
     {
@@ -28,24 +30,24 @@ export const obj=[
         productname:"Men Jeans || Jeans for Men || Cargo Jeans(Q-04-05)",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 223,
-        productcost: 779
-
+        productcost: 779,
+        type:'pant'
     },
     {
         productpic:'amazonpagepics/ethinicwear.jpg',
         productname:"Amzira Men's stylish Ethnic Wear Black Kurta Pajama",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 25,
-        productcost: 1999
-
+        productcost: 1999,
+        type:'pyjama'
     },
     {
         productpic:'amazonpagepics/purse.png',
         productname:"Men's Casual Cotton Poplin shirt(Regular Fit)",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 17,
-        productcost: 1049
-
+        productcost: 1049,
+        type:'purse'
     },
     {
         
@@ -53,111 +55,111 @@ export const obj=[
         productname:"Washing Machine||Clothes Washing||Portable",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 17,
-        productcost: 18999
-
+        productcost: 18999,
+        type:'washingmachine'
     },
     {
         productpic:'amazonpagepics/towels.png',
         productname:"Pack of Towels|| combo||Regular Use",
         productratingpic: 'amazonpagepics/rating-45.png',
         productrating: 1017,
-        productcost: 399
-
+        productcost: 399,
+        type:'towels'
     },
     {
         productpic:'amazonpagepics/fridge.png',
         productname:"LG Fridge|| 25litres Capacity||100watts",
         productratingpic: 'amazonpagepics/rating-45.png',
         productrating: 77,
-        productcost: 27999
-
+        productcost: 27999,
+        type:'fridge'
     },
     {
         productpic:'amazonpagepics/deter.png',
         productname:"Clothers perfume||Clothes Perfumeer||Jasmine favour",
         productratingpic: 'amazonpagepics/rating-30.png',
         productrating: 78,
-        productcost: 455
-
+        productcost: 455,
+        type:'detergent'
     },
     {
         productpic:'amazonpagepics/iron-machine.png',
         productname:"Iron machine||clothes free||weight 55kgs",
         productratingpic: 'amazonpagepics/rating-35.png',
         productrating: 448,
-        productcost: 1349
-
+        productcost: 1349,
+        type:'iron machine'
     },
     {
         productpic:'amazonpagepics/watch-2.png',
         productname:"Fastrack Watch||Men's Wear||Stylish",
         productratingpic: 'amazonpagepics/rating-45.png',
         productrating: 87,
-        productcost: 1249
-
+        productcost: 1249,
+        type:'watch'
     },
     {
         productpic:'amazonpagepics/handbag.png',
         productname:"Ladies Hand Bag||Regular Wear",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 87,
-        productcost: 785
-
+        productcost: 785,
+        type:'handbag'
     },
     {
         productpic:'amazonpagepics/kidswear.png',
         productname:"Kid's Casual ||shirt and pant combo||(Regular Fit)",
         productratingpic: 'amazonpagepics/rating-35.png',
         productrating: 46,
-        productcost: 549
-
+        productcost: 549,
+        type:'kidswear'
     },
     {
         productpic:'amazonpagepics/shoeblack.png',
         productname:"Men's Regular || Branded || Shoes || Adidas",
         productratingpic: 'amazonpagepics/rating-45.png',
         productrating: 8717,
-        productcost: 749
-
+        productcost: 749,
+        type:'shoe'
     },
     {
         productpic:'amazonpagepics/watch-2.png',
         productname:"Rolex Watch || Mens Wear || Daily use",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 2587,
-        productcost: 999
-
+        productcost: 999,
+        type:'watch'
     },
     {
         productpic:'amazonpagepics/detergent.png',
         productname:"Washing Detergent for Clothes || Anti microbial",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 54,
-        productcost: 349
-
+        productcost: 349,
+        type:'detergent'
     },
     {
         productpic:'amazonpagepics/shoe.png',
         productname:"Brocs Shoe||Mens wear||Daily use||Regular usage",
         productratingpic: 'amazonpagepics/rating-40.png',
         productrating: 92,
-        productcost: 1049
-
+        productcost: 1049,
+        type:'shoe'
     },
     {
         productpic:'amazonpagepics/soapperfume.png',
         productname:"Pack of soaps||Perfumes and || paste for Regular use",
         productratingpic: 'amazonpagepics/rating-45.png',
         productrating: 67,
-        productcost: 849
-
+        productcost: 849,
+        type:'soap'
     },
     {
         productpic:'amazonpagepics/towels.png',
         productname:"Regular Wear Towel",
-        productratingpic: 'amazonpagepics/rating-40.png',
-        productrating: 37,
-        productcost: 249
-
+        productratingpic: 'amazonpagepics/rating-45.png',
+        productrating: 567,
+        productcost: 549,
+        type:'towels'
     }
 ];
